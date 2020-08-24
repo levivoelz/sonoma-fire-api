eb228054608392fe2c09a4639c8ab4d4962a5dc9
Levi Voelz <levivoelz@gmail.com>
2020-08-23 22:17:45 -0700

    don't need things

--- app/controllers/wildfire.rb
+++ app/controllers/wildfire.rb
@@ -1,8 +1,8 @@
 require 'open-uri'
 
-get '/wildfire/:camera' do
-  url = "http://s3-us-west-2.amazonaws.com/alertwildfire-data-public/#{params[:camera]}/latest_full.jpg"
+get '/camera/:location' do
+  url = "http://s3-us-west-2.amazonaws.com/alertwildfire-data-public/#{params[:location]}/latest_full.jpg"
   referrer = 'http://www.alertwildfire.org/'
   headers("Content-Type" => "image/jpg")
-  body(open(url, "Referer" => referrer).read)
+  open(url, "Referer" => referrer).read
 end
