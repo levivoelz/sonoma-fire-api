dbf78ad3312e1884f309e2403747e13dbeecee67
Levi Voelz <levivoelz@gmail.com>
2020-08-23 22:03:58 -0700

    requiring stuff you need is sometimes good

--- app/controllers/wildfire.rb
+++ app/controllers/wildfire.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 get '/wildfire/:camera' do
   url = "http://s3-us-west-2.amazonaws.com/alertwildfire-data-public/#{params[:camera]}/latest_full.jpg"
   referrer = 'http://www.alertwildfire.org/'
