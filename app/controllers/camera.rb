da9adccc80c5fa1ba8b4108884c83e88164106c9
Levi Voelz <levivoelz@gmail.com>
2020-08-25 13:44:58 -0700

    redirect that shit

--- app/controllers/camera.rb
+++ app/controllers/camera.rb
@@ -1,8 +1,5 @@
 require 'open-uri'
 
 get '/camera/:location' do
-  url = "http://s3-us-west-2.amazonaws.com/alertwildfire-data-public/#{params[:location]}/latest_full.jpg"
-  referrer = 'http://www.alertwildfire.org/'
-  headers('Content-Type' => 'image/jpg')
-  open(url, 'Referer' => referrer).read
+  redirect "http://api.nvseismolab.org/camera/#{params[:location]}"
 end
