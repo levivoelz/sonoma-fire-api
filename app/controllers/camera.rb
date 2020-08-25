4879d9ba97e6f5aaf80018307e1893bb5d022453
Levi Voelz <levivoelz@gmail.com>
2020-08-25 10:42:54 -0700

    quotes

--- app/controllers/camera.rb
+++ app/controllers/camera.rb
@@ -3,6 +3,6 @@ require 'open-uri'
 get '/camera/:location' do
   url = "http://s3-us-west-2.amazonaws.com/alertwildfire-data-public/#{params[:location]}/latest_full.jpg"
   referrer = 'http://www.alertwildfire.org/'
-  headers("Content-Type" => "image/jpg")
-  open(url, "Referer" => referrer).read
+  headers('Content-Type' => 'image/jpg')
+  open(url, 'Referer' => referrer).read
 end
